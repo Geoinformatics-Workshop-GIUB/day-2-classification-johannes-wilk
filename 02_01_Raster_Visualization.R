0632aa8e919dad994d37783daeae4615f1edbea8
johannes-wilk <johannes.wilk97@gmx.de>
2021-10-12 11:46:05 +0200

    JW Part 1

--- 02_01_Raster_Visualization.R
+++ 02_01_Raster_Visualization.R
@@ -5,9 +5,9 @@
 library(raster)
 
 #Working directory
-setwd("~/Studium_nicht_Sciebo/RSRG/Teaching_material/Data")
-
-img <- brick("LC081960252020091901RT-SC20200925100850.tif")
+setwd("C:/Users/Johannes/Documents/1. Dokumente/3. Studium/2. Uni Bonn/1. Module/D2 R Kurs")
+#inserting Landsat image (it does not matter if cropped or original image)
+img <- brick("LC08_Bonn.tif")
 img
 
 #Plot
@@ -19,26 +19,27 @@ plotRGB(img,
 plotRGB(img,
         r = 4, g = 3, b = 2,
         stretch = "lin",
-        ext = extent(362699.321, 369890.701, 5619755.014, 5624836.560)
-)
+        )
 
-#Save subset
-img.subset_vis <- crop(img, extent(362699.321, 369890.701, 5619755.014, 5624836.560))
+#Save subset (nur wenn man das Bild vorher in R gecroppt hat)
+#img.subset_vis <- crop(img, extent(362699.321, 369890.701, 5619755.014, 5624836.560))
 
-writeRaster(img.subset_vis,
-            filename = "C081960252020091901RT-SC20200925100850_subset_vis.tif",
-            format = "GTiff",
-            overwrite = TRUE
-)
-
+#writeRaster(img.subset_vis,
+#            filename = "C081960252020091901RT-SC20200925100850_subset_vis.tif",
+#            format = "GTiff",
+#            overwrite = TRUE
+#)
+img.subset_vis <- brick("LC08_Bonn.tif")
 #Plot histogram/distribution
 green <- img.subset_vis[[3]]
-
+red <- img.subset_vis[[4]]
+NIR <- img.subset_vis[[5]]
 hist(green,
      breaks = 200,
-     xlim = c(0, 1500),
-     ylim = c(0, 15000),
+     xlim = c(0, 2000),
+     ylim = c(0, 20000),
      xlab = "band 3 reflectance value [DN * 0.01]",
      ylab = "frequency",
      main = "histogram L8 band 3 (green)"
 )
+
